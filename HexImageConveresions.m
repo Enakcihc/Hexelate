965377f790166158d1a5e89ae5f1657aebb25249
Aaron Benson Wong <a.wong@erasmusmc.nl>
2023-12-31 15:30:28 +0100

    updated demo
    included sample image, updated .m code, small update to readme (still incomplete)

--- HexImageConveresions.m
+++ HexImageConveresions.m
@@ -7,42 +7,30 @@
 %
 % Written in Matlab R2019a.
 
-%% 
+%% Clear workspace and load the image
 clear; close all
-ImgMat = imread('Kek-Look-Tong-square.JPG'); hexWidthPx = 25;
+ImgMat = imread('sample\Leonardo-Mona-Lisa.jpg');hexWidthPx = 30;
 
 %% Coordinate of image pixels
 xDim = size(ImgMat,2); yDim = size(ImgMat,1);
 nPixel = xDim * yDim;
 xx = 1:xDim; yy = 1:yDim;
-% ImgPx = struct([]);
 ImgPx.pxy = combvec(xx,yy); % xy coordinates of all pixel in image. Upper left pixel: (1,1)
-ImgPx.qrs = p2h(ImgPx.pxy); % pixel positions in cubic coordinates
-% ImgPx = struct( 'x',num2cell(ImgPx.pxy(1,:)),...
-%                 'y',num2cell(ImgPx.pxy(2,:)),...
-%                 'q',num2cell(qrs(1,:)),...
-%                 'r',num2cell(qrs(2,:)),...
-%                 's',num2cell(qrs(3,:)) ...
-%                 );
 clear('xx','yy');
-%% calculate downsampled grid that spans the image
 
-% grid.qrs = unique(cube_round(ImgPx.qrs)','row')';
-% nHexel = size(grid.qrs,2);
-% grid.pxy = h2p(grid.qrs);
-disp(['Number of pixels: ',num2str(nPixel), ' (',num2str(xDim),' x ',num2str(yDim),')'])
-% disp(['Number of hexels: ',num2str(nHexel)])
-% expand grid
-samp.qrs = unique(cube_round(ImgPx.qrs ./ hexWidthPx)','row')';
-samp.pxy = h2p(samp.qrs)*hexWidthPx;
+%% calculate downsampled grid that spans the image
+samp.cr = unique(dw_round(p2dw(ImgPx.pxy)./ hexWidthPx)','row','stable')';
+samp.pxy = dw2p(samp.cr)*hexWidthPx;
 % exclude out of bound hexels
 sel = samp.pxy(1,:) <= xDim & samp.pxy(2,:) <= yDim & samp.pxy(1,:) > 0 & samp.pxy(2,:) > 0;
-samp.qrs = samp.qrs(:,sel);samp.pxy = samp.pxy(:,sel);
-% get column-row (doublewidth) representation
-samp.cr = ax2dw(samp.qrs);
-nSamp = size(samp.qrs,2);
+samp.pxy = samp.pxy(:,sel);
+samp.cr = samp.cr(:,sel);samp.qrs = dw2cube(samp.cr);
+nSamp = size(samp.pxy,2);
+
+disp(['Number of pixels: ',num2str(nPixel), ' (',num2str(xDim),' x ',num2str(yDim),')'])
 disp(['Number of downsampled hexels: ',num2str(nSamp)])
-clear('grid','sel');
+clear('sel');
+
 %% calculate output size
 % specify canvas size
 canvasWidth = 50; % cm
@@ -52,32 +40,35 @@ minRow = min(samp.cr(2,:)); maxRow = max(samp.cr(2,:));
 nCols = 0.5 * (maxCol - minCol) + 1;
 nRows = (maxRow - minRow) + 1;
 
+disp(['Number of columns: ',num2str(nCols),...
+      '   Number of rows: ',num2str(nRows)]);
+
 % distance between centers of adjacent hexels
-margin = 0.25; %
-a = sqrt(3) / 2;
+margin = 0.25; % fraction of 1 Hex
+a = sqrt(3) / 2; % vertical distance between adjacent rows
 HexDist1 = canvasWidth / (nCols+2*margin); % 2 * margin on both sides
-HexDist2 =  canvasHeight / (nRows+2*margin) /  a; %distance between centers of adjacent hexels
+HexDist2 =  canvasHeight / (nRows+2*margin) /  a; % 2 * margin on both sides
 HexDist = min(HexDist1,HexDist2);
 
 canvasXOffset = 0.5 * canvasWidth - 0.5*(nCols-1)*HexDist;
 canvasYOffset = 0.5 * canvasHeight - 0.5*(nRows-1)*a*HexDist;
-minPntXY = HexDist*h2p(dw2ax([minCol;minRow]));
+minPntXY = HexDist*h2p(dw2cube([minCol;minRow]));
 canvasOffset = [canvasXOffset;canvasYOffset] - minPntXY;
 
-samp.xy = HexDist*h2p(samp.qrs)+canvasOffset;
+samp.xy = HexDist*dw2p(samp.cr)+canvasOffset;
 
 %% sampling pixel intensity
 % & generate pixelation image
 nCh = size(ImgMat,3);
-CData_samp = nan(size(samp.pxy,2),nCh);
-CData_mean = nan(size(samp.pxy,2),nCh);
+samp.c_samp = nan(size(samp.pxy,2),nCh);
+samp.c_mean = nan(size(samp.pxy,2),nCh);
 HexelateImg_samp = nan(size(ImgMat));
 HexelateImg_mean = nan(size(ImgMat));
 
 % nearest central pixel
 tic
 for ii = 1:size(samp.pxy,2)
-    CData_samp(ii,:) = squeeze(ImgMat(round(samp.pxy(2,ii)),round(samp.pxy(1,ii)),:));
+    samp.c_samp(ii,:) = squeeze(ImgMat(round(samp.pxy(2,ii)),round(samp.pxy(1,ii)),:));
 end
 disp('Sampling done.')
 toc
@@ -85,205 +76,153 @@ toc
 % mean 
 tic
 
-mapHex = cube_round(ImgPx.qrs ./ hexWidthPx);
+% mapHex = cube_round(ImgPx.qrs ./ hexWidthPx);
+mapHex = dw_round(p2dw(ImgPx.pxy) ./ hexWidthPx);
+
 for ii = 1:size(samp.pxy,2)
-    sel = all(samp.qrs(:,ii) == mapHex,1);
-%     subplot(1,3,2)
-%     scatter(ImgPx.pxy(1,sel),ImgPx.pxy(2,sel))
+    sel = all(samp.cr(:,ii) == mapHex,1);
     nn = sum(sel);
     for ch = 1:nCh
         linearInd = sub2ind([yDim,xDim,nCh], ImgPx.pxy(2,sel), ImgPx.pxy(1,sel),ch*ones(1,nn));
-        CData_mean(ii,ch) = mean(ImgMat(linearInd),'all');
-        HexelateImg_samp(linearInd) =CData_samp(ii,ch);
-        HexelateImg_mean(linearInd) =CData_mean(ii,ch);
+        samp.c_mean(ii,ch) = mean(ImgMat(linearInd),'all');
+%         HexelateImg_samp(linearInd) =samp.c_samp(ii,ch);
+%         HexelateImg_mean(linearInd) =samp.c_mean(ii,ch);
     end
 end
 disp('Averaging done.')
 toc
 
+clear('ii','nn','sel')
 
+%% outputs
 
-%% output to JSON
-
-ch = 2;
-fileID = fopen('Hexelated.json','w');
-JSON = jsonencode(table(samp.pxy(1,:)',samp.pxy(2,:)',...
+outTable = table(samp.pxy(1,:)',samp.pxy(2,:)',...
                 samp.qrs(1,:)',samp.qrs(2,:)',samp.qrs(3,:)',...
                 samp.cr(1,:)',samp.cr(2,:)',...
-                CData_mean(:,ch),...
-                'VariableNames',{'px','py','q','r','s','col','row','c'}));
+                samp.c_mean(:,ch),samp.c_samp(:,ch),...
+                'VariableNames',{'px','py','q','r','s','col','row','c_mean','c_samp'});
+
+% output to JSON
+
+ch = max(2,nCh);
+fileID = fopen('Hexelated.json','w');
+JSON = jsonencode(outTable);
 fwrite(fileID,JSON);
 fclose(fileID);
-%% show image on Canvas dimension
-fig = figure;
-screenScale = 7; % "points" / cm
-figSize = screenScale*[canvasWidth,canvasHeight]; figMargin = [50,50];
-fig.Units = 'points';
-fig.Position = [300,50,(figSize+2*figMargin)];
-nSteps = 8;
-ax = gca;
-ax.Units = 'points';
-ax.Position = [figMargin,figSize];
-ch = 2;
-maxMrkrSize = pi * (0.5*(HexDist*screenScale))^2; % somehow pi*r^2 is not correct
-maxMrkrSize = (HexDist*screenScale)^2; % using the area of a square works 
-sz = maxMrkrSize*szLU(-CData_mean(:,ch),nSteps);
-scatter(samp.xy(1,:),samp.xy(2,:),sz,'filled','Marker','o');
-set(gca,'YDir','reverse');
-ylim([0,canvasHeight]);xlim([0,canvasWidth]);
-ylabel('cm')
-xlabel('cm')
-%% Show images (R,G,B, RGB)
-fig = figure;
-fig.Position = [300,50,750,750];
-Colors = {'r','g','b'};%{'c','m','y'};
-nSteps = 8;
-for ch = 1:nCh
-    subplot(2,2,ch)
-        sz = 10*szLU(-CData_mean(:,ch),nSteps);%round((280-CData_mean(:,ch))./64)+eps;
 
-%     scatter(samp.pxy(1,:),samp.pxy(2,:),sz,Colors{ch},'filled');
-    scatter(samp.pxy(1,:),samp.pxy(2,:),sz,'filled');
-    set(gca,'YDir','reverse');
-%     axis square
-        ylim([0,yDim+1]);xlim([0,xDim+1]);
-    title(Colors{ch});
-end
-subplot(2,2,4)
-scatter(samp.pxy(1,:),samp.pxy(2,:),9,CData_mean./255,'filled');
-set(gca,'YDir','reverse');
-    ylim([0,yDim+1]);xlim([0,xDim+1]);
+% output as CSV
+writetable(outTable,'Hexalated.csv');
 
-% axis square
-
-fig.PaperUnits = 'inches';
-fig.PaperSize = fig.Position(3:4)./96; %96 dpi
-% saveas(fig,'sampOutput.pdf')
-%% demo
-
-% calculate axis labels
-sel = samp.cr(2,:) == min(samp.cr(2,:));
-ColLbl_x = samp.pxy(1,sel);
-ColLbls = samp.cr(1,sel);
-sel = samp.cr(1,:) == min(samp.cr(1,:));
-RowLbl_x = samp.pxy(2,sel);
-RowLbls = samp.cr(2,sel);
-[RowLbls,idx] = sort(RowLbls);
-RowLbl_x = RowLbl_x(idx);
-
-clear('sel','idx');
-
-
-fig = figure;
-fig.Position = [200,300,900,300];
-
-nSteps = 8;
-
-% original image
-subplot(1,3,1)
-    imagesc(ImgMat);
-    axis square
-    
-% scaled
-    ch = 3; % channel to use
-    dotScale = 2;
-    useMean = 0; % 1 = mean, 0 = decimated samples
-    sz_mean = (nSteps-1)*szLU(-CData_mean(:,ch),nSteps);%round((280-CData_mean(:,ch))./32);
-    sz_samp = (nSteps-1)*szLU(-CData_samp(:,ch),nSteps);%round((280-CData_samp(:,ch))./32);
-    if(useMean); sz = sz_mean; else; sz = sz_samp; end
-    [uSz,ia,ic] = unique(sz);
-    nColors = length(uSz);
-%     CScale = colorcube(nColors);
-    CScale = 0.8.*jet(nColors);
-%     CScale = lines(nColors);
-    c = CScale(ic,:);
-    subplot(1,3,2)
-    scatter(samp.pxy(1,:),samp.pxy(2,:),dotScale*sz,c,'filled');
-    set(gca,'YDir','reverse');
-    ylim([0,yDim]);xlim([0,xDim]);
-    axis square
-
-    subplot(1,3,3)
-    scatter(samp.pxy(1,:),samp.pxy(2,:),15,c,'filled');
-%         scatter(samp.pxy(1,:),samp.pxy(2,:),sz_mean,'filled');
-
-    set(gca,'YDir','reverse');
-    ylim([0,yDim]);xlim([0,xDim]);
-    axis square
-colormap(CScale);
-caxis([min(uSz)-.5,max(uSz)+.5])
-colorbar('Ticks',uSz,'Position',[.92,.3,.02,.5]);
-
-% ticks
-xticks(ColLbl_x);xticklabels(ColLbls);
-yticks(RowLbl_x);yticklabels(RowLbls);
-
-% label extremes
-locExtremes = nan(4,1);
-[~,locExtremes(1)] = min([1,1]*samp.pxy,[],2);
-[~,locExtremes(2)] = min([1,-1]*samp.pxy,[],2);
-[~,locExtremes(3)] = max([1,1]*samp.pxy,[],2);
-[~,locExtremes(4)] = max([1,-1]*samp.pxy,[],2);
-
-% locExtremes = union(locMinCR,locMaxCR);
-for ii = 1:length(locExtremes)
-    idx = locExtremes(ii);
-    text(samp.pxy(1,idx),samp.pxy(2,idx),['(',num2str(samp.cr(1,idx)),',',num2str(samp.cr(2,idx)),')'],...
-        'FontSize',8);
-end
-
-fig.PaperUnits = 'inches';
-fig.PaperSize = fig.Position(3:4)./96; %96 dpi
-% saveas(fig,'sampOutput.pdf')
-saveas(fig,'sampOutput_mean.png')
-
-%% Instruction
-fig = figure;
-fig.Position = [100,100,1200,500];
-    dotScale = 12;
-    subplot(1,2,1)
-    scatter(samp.pxy(1,:),samp.pxy(2,:),dotScale*sz,'filled');
-    set(gca,'YDir','reverse');
-    ylim([0,yDim]);xlim([0,xDim]);
-    axis square
-    subplot(1,2,2)
-    scatter(samp.pxy(1,:),samp.pxy(2,:),dotScale*nSteps/2,c,'filled');
-
-    set(gca,'YDir','reverse');
-    ylim([0,yDim]);xlim([0,xDim]);
-    
-    
-    axis square
-colormap(CScale);
-caxis([min(uSz)-.5,max(uSz)+.5])
-colorbar('Ticks',uSz,'Position',[.92,.3,.02,.5]);
-
-% label extremes
-locExtremes = nan(4,1);
-[~,locExtremes(1)] = min([1,1]*samp.pxy,[],2);
-[~,locExtremes(2)] = min([1,-1]*samp.pxy,[],2);
-[~,locExtremes(3)] = max([1,1]*samp.pxy,[],2);
-[~,locExtremes(4)] = max([1,-1]*samp.pxy,[],2);
-
-% locExtremes = union(locMinCR,locMaxCR);
-for ii = 1:length(locExtremes)
-    idx = locExtremes(ii);
-    text(samp.pxy(1,idx),samp.pxy(2,idx),['(',num2str(samp.cr(1,idx)),',',num2str(samp.cr(2,idx)),')'],...
-        'FontSize',8);
-end
-
-fig.PaperUnits = 'inches';
-fig.PaperSize = fig.Position(3:4)./96; %96 dpi
-% saveas(fig,'instruction.pdf')
-%% Hexelate Images
+%% Show Hexelated Images
 fig = figure;
 fig.Position = [200,300,900,250];
     subplot(1,3,1)
-    imagesc(ImgMat); axis square;
+    imagesc(ImgMat); axis square; title('original')
     subplot(1,3,2)
-    image(HexelateImg_samp./255); axis square;
+    image(HexelateImg_samp./255); axis square; title('sampled (decimate)')
     subplot(1,3,3)
-    image(HexelateImg_mean./255); axis square;
+    image(HexelateImg_mean./255); axis square; title('mean')
+
+%% show image on Canvas dimension
+% parameters
+screenScale = 7; % points / cm; for display purpose
+nSteps = 256;
+ch = 2;%max(1,nCh); % which channel to use (1=R,2=G,3=B)
+Color = [105,81,146]./255;
+
+% --- setup figure ---
+    fig = figure;
+    figSize = screenScale*[canvasWidth,canvasHeight]; figMargin = [50,50];
+    fig.Units = 'points';
+    fig.Position = [300,50,(figSize+2*figMargin)];
+    
+    ax = gca;
+    ax.Units = 'points';
+    ax.Position = [figMargin,figSize];
+
+% --- calculate marker size ---
+    % maxMrkrSize = pi * (0.5*(HexDist*screenScale))^2; % somehow pi*r^2 is not correct
+    maxMrkrSize = (HexDist*screenScale)^2; % using the area of a square works 
+    sz = szLU(max(-samp.c_mean(:,ch),-200),nSteps); % reversed scale so darker hexels have larger dots
+
+% --- plot dots ---
+    scatter(samp.xy(1,:),samp.xy(2,:),maxMrkrSize.*sz,Color,'filled','Marker','o');
+
+    set(gca,'YDir','reverse');
+    ylim([0,canvasHeight]);xlim([0,canvasWidth]);
+    ylabel('cm');xlabel('cm')
+    
+
+%     scatter(samp.pxy(1,:),samp.pxy(2,:),dotScale*nSteps/2,c,'filled');
+%% Instruction for Dot artwork
+screenScale = 4; % points / cm; for display purpose
+nSteps = 8;
+ch = max(2,nCh); % which channel to use (1=R,2=G,3=B)
+Color = [0,0,0]; % color of dots
+useMean = 1;
+
+
+
+
+
+fig = figure;
+    axSize = screenScale*[canvasWidth,canvasHeight];
+    figSize = [2,1].*axSize; figMargin = [50,50];
+    fig.Units = 'points';
+    fig.Position = [10,10,(figSize+[2+1,2].*figMargin)];
+    
+% output picture
+    ax = subplot(1,2,1);
+    ax.Units = 'points';
+    ax.Position = [figMargin,axSize];
+    maxMrkrSize = (HexDist*screenScale)^2; % using the area of a square works 
+    if(useMean)
+        sz = szLU(-samp.c_mean(:,ch),nSteps);
+    else
+        sz = szLU(-samp.c_samp(:,ch),nSteps);
+    end
+    % --- create color lookup
+        [uSz,ia,ic] = unique((nSteps-1).*sz);
+        nColors = length(uSz);
+        CScale = 0.8.*jet(nColors);
+        c = CScale(ic,:);
+    
+    scatter(samp.xy(1,:),samp.xy(2,:),maxMrkrSize*sz,'filled','Marker','o','MarkerFaceColor',Color);
+    set(gca,'YDir','reverse');
+    ylim([0,canvasHeight]);xlim([0,canvasWidth]);
+    ylabel('cm');xlabel('cm')
+
+    ax = subplot(1,2,2);
+    ax.Units = 'points';
+    ax.Position = [[2,1].*figMargin+[1,0].*axSize,axSize];
+    scatter(samp.xy(1,:),samp.xy(2,:),maxMrkrSize,c,'filled');
+
+    set(gca,'YDir','reverse');
+    ylim([0,canvasHeight]);xlim([0,canvasWidth]);
+    ylabel('cm');xlabel('cm')
+    
+    colormap(CScale);
+    caxis([min(uSz)-.5,max(uSz)+.5])
+    colorbar('Ticks',uSz,'Position',[.92,.3,.02,.5]);
+
+% label extremes
+    locExtremes = nan(4,1);
+    [~,locExtremes(1)] = min([1,1]*samp.pxy,[],2);
+    [~,locExtremes(2)] = min([1,-1]*samp.pxy,[],2);
+    [~,locExtremes(3)] = max([1,1]*samp.pxy,[],2);
+    [~,locExtremes(4)] = max([1,-1]*samp.pxy,[],2);
+    HorAlign = {'left','left','left','left'};
+    VerAlign = {'bottom','top','top','bottom'};
+
+    for ii = 1:length(locExtremes)
+        idx = locExtremes(ii);
+        text(samp.xy(1,idx),samp.xy(2,idx),['(',num2str(samp.cr(1,idx)),',',num2str(samp.cr(2,idx)),')'],...
+            'FontSize',8,'HorizontalAlignment',HorAlign{ii},'VerticalAlignment',VerAlign{ii});
+    end
+% setup and save output file
+fig.PaperUnits = 'inches';
+fig.PaperSize = fig.Position(3:4)./72; %72 points per inch
+saveas(fig,'instruction.pdf')
 
 %%
 % here, size of 1 is defined as distance between centers of adjacent
@@ -309,6 +248,22 @@ function qrs = p2h(xy)
     qrs = [qr;-sum(qr,1)];
 end
 
+function cr = p2dw(xy)
+	% doublewidth
+	% xy: 2xn matrix containing [px;py]
+    A = [   2,   0;...
+            0,   2/sqrt(3) ];
+    cr = A * xy;
+end
+
+function xy = dw2p(cr)
+     % doublewidth
+     % cr: 2xn matrix containing [cols;rows]
+    A = [   0.5,   0;...
+            0,   0.5*sqrt(3) ];
+    xy = A * cr;
+end
+
 function dist = cube_distance(a, b)
     dist = sum(abs(a-b),1) / 2;
 end
@@ -318,22 +273,25 @@ function cr = ax2oddr(hex)
     row = hex(2,:);
     cr = [col;row];
 end
-function cr = ax2dw(hex) % axial_to_doublewidth
+function cr = cube2dw(hex) % axial_to_doublewidth
     col = 2 * hex(1,:) + hex(2,:);
     row = hex(2,:);
     cr = [col;row];
 end
-function hex = oddr2ax(cr)
+function hex = oddr2cube(cr)
     q = cr(1,:) - (cr(2,:) - bitand(cr(2,:),1)) / 2;
     r = cr(2,:);
     hex = [q;r;-q-r;];
 end
-function hex = dw2ax(cr) % axial_to_doublewidth
+function hex = dw2cube(cr) % axial_to_doublewidth
     q = (cr(1,:) - cr(2,:)) / 2;
     r = cr(2,:);
     hex = [q;r;-q-r;];
 end
 
+function cr = dw_round(frac)
+    cr = cube2dw(cube_round(dw2cube(frac)));
+end
 function qrs = cube_round(frac)
 %     var q = round(frac.q)
 %     var r = round(frac.r)
--- README.md
+++ README.md
@@ -1,11 +1,38 @@
-# Introduction
+# What is Hexelate?
 
 The name "Hexelate" is a wordplay combining the words "hexagon" and "pixelate". This is a small set of code (currently only in Matlab) that takes a digital image, samples its intensities in a hexagonal grid, and then draws colored dots of various sizes at the grid points to create a "downsampled(pixelated)" representation of the image. Alternatively, an image drawn with hexagons of different intensity/color can be created. 
 
 
-# Background of this code
+# Why did I wrote Hexelate and who may want to use it?
 
 I wrote this code in 2022 to aid create a physical art work for my wife's birthday. I used a family portrait as the input and stamped dots of various sizes on a canvas using items like pens, a permanent marker and a coin. Thus the section of "instruction" which color-codes the different dot size.
+
+Hexelate is useful for anybody who wants to generate a similar kind of hexelated images or artwork with their own picture.
+
+# Instructions
+
+## Installation
+This code is written as a Matlab script as a single .m file. All custom functions are included as local functions at the bottom of file. To run this you will need Matlab installed on your computer. Simply download the file "HexImageConversions.m" to a directory of your choice and open it in Matlab. 
+
+If you are not familiar with the idea of a script you can read more [here](https://www.mathworks.com/help/matlab/learn_matlab/scripts.html)
+
+## Generate a dot image with canvas dimensions
+Open the file "HexImageConversions.m" in Matlab. Run the cells one by one by the "Run and Advance." button in the editor, or by pressing Ctrl+Enter (Windows) or Replace the path to the example image with the path to your own image. Like this:
+
+``` Matlab
+ImgMat = imread('\path\to\your\own\image.JPG'); hexWidthPx = 30;
+```
+
+The variable ```hexWidthPx``` determines how big the hexagons will be (in pixels of the original image). This is the distance between the center of neighboring hexagons, or equivalently the distance between opposite sides of the hexagons.
+
+In the third cell calculate output size, you can specify the size of the canvas this image is supposed to occupy by adjusting ```canvasWidth``` and ```canvasHeight```. 
+
+``` Matlab
+%% calculate output size
+% specify canvas size
+canvasWidth = 50; % cm
+canvasHeight = 50; % cm
+```
 
 
 # Credit
